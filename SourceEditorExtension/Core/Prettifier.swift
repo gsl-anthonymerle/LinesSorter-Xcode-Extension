bf5554f80500f5b6647cbc2981eaaa629cb22904
gsl-anthonymerle <anthony.merle@groupeseloger.com>
2024-10-24 09:38:23 +0200

    Revert "Sort lines without taking into account keywords and case sensitivity"
    This reverts commit 398784bda9e3d334cfdd52952d77f64a53542c3a.

--- SourceEditorExtension/Core/Prettifier.swift
+++ SourceEditorExtension/Core/Prettifier.swift
@@ -24,17 +24,8 @@ struct Prettifier {
         let linesRemoved = removeBlankLines(lines: lines, in: range)
         range = range.lowerBound...(range.upperBound - linesRemoved)
         
-        LinesSorter().sort(lines, in: range) { lhs, rhs in
-            let lhs = lhs
-                .removingImportKeywords()
-                .lowercased()
-            let rhs = rhs
-                .removingImportKeywords()
-                .lowercased()
-
-            return lhs < rhs
-        }
-
+        LinesSorter().sort(lines, in: range, by: <)
+        
         removeDuplicates(from: lines, in: range)
     }
     
@@ -63,13 +54,5 @@ struct Prettifier {
             .duplicatedElementsIndices
             .map { $0 + range.lowerBound }
             .forEach(lines.removeObject(at:))
-    }
-}
-
-extension StringProtocol {
-    func removingImportKeywords() -> String {
-        replacingOccurrences(of: "@preconcurrency", with: "")
-            .replacingOccurrences(of: "@testable", with: "")
-            .replacingOccurrences(of: "import", with: "")
     }
 }
